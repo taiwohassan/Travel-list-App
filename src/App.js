bf7d9f35d0c85ca43848364b865f39eca1b3133b
akinyemihassan <akinyemitaiwo646@gmail.com>
2024-04-18 12:19:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import  ParkingList from "./components/ParkingList";
+import Stats from "./components/Stats";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialItems = [
+
+
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
+export default function App() {
+  const[items, setItems]=useState([])
+
+
+  function handleAddItems(item){
+    setItems((items)=>[...items, item])
+   }
+
+   function handleDelete(id){
+    console.log(id)
+    setItems(items=>items.filter(item=>item.id !== id))
+   }
+
+  
+
+   function handleToggleItem(id){
+    setItems(items=> items.map(item=>item.id=== id ? {...item, packed:!item.packed}: item))
+
+   }
+
+   function handleClearList(){
+    const confirmed=window.confirm('Are you sure you want to delete all items?')
+      if (confirmed) setItems([])
+   }
+  return <div className="app">
+    <Logo/>
+    <Form onAddItems={handleAddItems}/>
+    <ParkingList items={items} 
+    onDeleteItem={handleDelete}
+    onToggleItems={handleToggleItem}
+    onClearList={handleClearList}/>
+    <Stats items={items}/>
+  </div>
 }
 
-export default App;
+
+
---
+++ src/components/Logo.jsx
@@ -0,0 +1,5 @@
+export default function Logo(){
+    return <h1>🌴 Explore Beyond 💼</h1>
+  }
+
+ 
